81c34c4c9ca128d0aba542c1a5d4be77f98625d4
Robbie Bart <adm@AdminisatorsAir.lan>
2020-06-18 14:13:13 -0400

    q1.0 done

--- samples/balloon/balloon.js
+++ samples/balloon/balloon.js
@@ -1,1 +1,32 @@
-const balloon = document.getElementById('balloon');
+const balloon = document.querySelector("#balloon");
+// declaring variables/settings
+let balloonSize = 12;
+balloon.style.fontSize = `${balloonSize}px`;
+const RATE = 5;
+const LIMIT = 75;
+// listen on the up and down arrows
+// if balloon is bigger than x, BOOM
+const handleKeydown = function (event) {
+  const keyPress = event.key;
+  if (keyPress === "ArrowUp") {
+    balloonSize += 10;
+    balloon.style.fontSize = `${balloonSize}px`;
+  }
+  if (keyPress === "ArrowDown") {
+    balloonSize -= 10;
+    balloon.style.fontSize = `${balloonSize}px`;
+  }
+  if (balloonSize > LIMIT) {
+    balloon.innerText = ":boom:";
+    const explosion = setInterval(function () {
+      balloonSize += 50;
+      balloon.style.fontSize = `${balloonSize}px`;
+      if (balloonSize > 1000) {
+        balloon.style.opacity = 0;
+        clearInterval(explosion);
+      }
+    }, 20);
+    document.removeEventListener("keydown", handleKeydown);
+  }
+};
+document.addEventListener("keydown", handleKeydown);
--- workshop/exercise-1.0/app.js
+++ workshop/exercise-1.0/app.js
@@ -1,3 +1,21 @@
 // Exercise 1.0
 // ------------
-console.log('exercise-1');
+// console.log('exercise-1');
+
+const body = document.querySelector("body");
+
+const game = function () {
+  console.log("exercise-1");
+  body.innerText = "You win!";
+  body.appendChild(main);
+};
+
+body.addEventListener("click", game);
+
+/* notes for self: the event listener runs function game 
+when a click happens, you do not need an if; line 5
+selects the whole body, line 13 sets the condition, 
+and the code in const runs only if that condition 
+happens
+
+*/
